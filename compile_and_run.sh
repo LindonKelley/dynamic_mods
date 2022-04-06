a00e5734920cab6d961cb62f83951693e340ce30
lindon <53193460+LindonKelley@users.noreply.github.com>
2022-04-06 16:28:05 -0500

    moved mods into mods/ subfolder, updated compiling helper script

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-echo "building loader"
+echo "compiling loader"
 cargo build --manifest-path loader/Cargo.toml || exit
-echo "building mod a"
-cargo build --manifest-path mod_a/Cargo.toml || exit
-echo "building mod b"
-cargo build --manifest-path mod_b/Cargo.toml || exit
-echo "building mod c"
-cargo build --manifest-path mod_c/Cargo.toml || exit
-
+rm -r "output" 2>/dev/null
 mkdir -p "output/mods"
-(
-mv loader/target/debug/loader output/
-mv mod_a/target/debug/libmod_a.so output/mods/mod_a.so
-mv mod_b/target/debug/libmod_b.so output/mods/mod_b.so
-mv mod_c/target/debug/libmod_c.so output/mods/mod_c.so
-) 2>/dev/null
+mv loader/target/debug/loader output/ 2>/dev/null
+
+echo "compiling mods"
+for mod in "$@"; do
+  [ -d "mods/$mod" ] || { echo "mods/$mod does not exist or is not a directory"; exit; }
+  if [ -f "mods/$mod/Cargo.toml" ]; then
+    echo "compiling $mod via Rust"
+    cargo build --manifest-path mods/"$mod"/Cargo.toml || exit
+    mv mods/"$mod"/target/debug/lib"$mod".so output/mods/"$mod".so 2>/dev/null
+  else
+    echo "mods/$mod/Cargo.toml not found, unable to compile $mod"
+    exit
+  fi
+done
+
 exec ./output/loader
